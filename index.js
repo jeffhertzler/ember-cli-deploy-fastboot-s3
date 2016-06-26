dde41a0a96fe8b81887a00535f6cdde377f271e1
Jeff Hertzler <jeffhertzler@gmail.com>
2016-06-26 04:38:46 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 /* jshint node: true */
 'use strict';
 
+var FastbootS3DeployPlugin = require('./lib/fastboot-s3-deploy-plugin');
+
 module.exports = {
-  name: 'ember-cli-deploy-fastboot-s3'
+  name: 'ember-cli-deploy-fastboot-s3',
+
+  createDeployPlugin: function(options) {
+    return new FastbootS3DeployPlugin({
+      name: options.name
+    });
+  }
 };
